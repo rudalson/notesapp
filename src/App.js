dde8f576d283568c9182a09b21e27fb132510ede
rudalson <rudalson@gmail.com>
2022-06-28 18:27:41 +0900

    added : query notes through appsync

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import { useEffect, useReducer } from 'react';
+import { API } from 'aws-amplify';
+import { List } from 'antd';
+
+import 'antd/dist/antd.css';
+import { listNotes } from './graphql/queries';
 import './App.css';
 
+const styles = {
+  container: { padding: 20 },
+  item: { textAlign: 'left' },
+};
+
+const initialState = {
+  notes: [],
+  loading: true,
+  error: false,
+  form: { name: '', description: '' },
+};
+
 function App() {
+  function reducer(state, action) {
+    switch (action.type) {
+      case 'SET_NOTES':
+        return { ...state, notes: action.notes, loading: false };
+      case 'ERROR':
+        return { ...state, loading: false, error: true };
+      default:
+        return state;
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  async function fetchNotes() {
+    try {
+      const notesData = await API.graphql({ query: listNotes });
+      dispatch({ type: 'SET_NOTES', notes: notesData.data.listNotes.items });
+    } catch (err) {
+      console.log('error: ', err);
+      dispatch({ type: 'ERROR' });
+    }
+  }
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  function renderItem(item) {
+    return (
+      <List.Item style={styles.item}>
+        <List.Item.Meta title={item.name} description={item.description} />
+      </List.Item>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' style={styles.container}>
+      <List
+        loading={state.loading}
+        dataSource={state.notes}
+        renderItem={renderItem}
+      />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Amplify } from 'aws-amplify';
+
 import './index.css';
 import App from './App';
+import config from './aws-exports';
 import reportWebVitals from './reportWebVitals';
+
+Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
